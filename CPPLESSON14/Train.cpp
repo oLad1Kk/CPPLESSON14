f5ff5071c84227ec403c9bd89ef1130cd82b1e24
Vlad <vasvladikus@gmail.com>
2024-12-12 16:51:43 +0100

    class Train

--- CPPLESSON14/Train.cpp
+++ CPPLESSON14/Train.cpp
@@ -1,1 +1,35 @@
 #include "Train.h"
+
+Train::Train()
+{
+    this->number = "undefined";
+    this->station = "undefined";
+    this->departureTime = "undefined";
+}
+
+Train::Train(string number, string station, string departureTime)
+{
+    this->number = number;
+    this -> station = station;
+    this -> departureTime = departureTime;
+}
+
+void Train::setDepartureTime(string departureTime)
+{
+    this->departureTime = departureTime;
+}
+
+string Train::getNumber() const
+{
+    return number;
+}
+
+string Train::getStation() const
+{
+    return station;
+}
+
+string Train::getDepartureTime() const
+{
+    return departureTime;
+}
